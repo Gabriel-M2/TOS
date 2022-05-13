eba2eade79ff6596515be6b1045faaf94900d046
Gabriel-M2 <gmontenegro1227@gmail.com>
2022-05-13 18:32:10 -0300

    Adicionando funções na NavBar

--- src/Components/Links.tsx
+++ src/Components/Links.tsx
@@ -12,10 +12,42 @@ export function Links() {
                     SOBRE-NÓS <Bus className="w-6 h-6 ml-3" />
                     
                 </span>
-                <span className="flex m-2 items-center px-5 rounded-full hover:bg-orange-500 transition-all ease-in-out delay-150  cursor-pointer border-2 border-orange-500">
-                    SERVIÇOS <CaretUp  className="w-6 h-6 ml-3 hover:rotate-180 hover:transition-all ease-in-out delay-150" />
+                <Popover className="flex">
+
+                <Popover.Panel className="bg-zinc-800 absolute top-24 right-44 p-5 rounded-xl">
+
+                        <span className="flex m-2 items-center justify-center p-3  px-5 rounded-xl group hover:bg-orange-500 transition-all ease-in-out delay-150  cursor-pointer border-2 border-orange-500">
+                            Lorem Ipsum
+                        </span>
+                        <span className="flex m-2 items-center justify-center p-3  px-5 rounded-xl group hover:bg-orange-500 transition-all ease-in-out delay-150  cursor-pointer border-2 border-orange-500">
+                            Lorem Ipsum 
+                        </span>
+                        <span className="flex m-2  items-center p-3 justify-center px-5 rounded-xl hover:bg-orange-500 transition-all ease-in-out delay-150  cursor-pointer border-2 border-orange-500">
+                            Lorem Ipsum 
+                        </span>
+                    </Popover.Panel>
+
+                    <Popover.Button>
+                    <span className="flex m-2 items-center group p-3 px-5 rounded-full hover:bg-orange-500 transition-all ease-in-out delay-150  
+                cursor-pointer border-2 border-orange-500  overflow-hidden ">
+                    <div className="flex">
+                        SERVIÇOS <CaretUp  className="w-6 h-6 ml-3 group-hover:rotate-180 hover:transition-all ease-in-out delay-150" />
+                    </div>
                     
                 </span>
+                    </Popover.Button> 
+                </Popover>
+
+                {/* <span className="flex m-2 items-center group  px-5 rounded-full hover:bg-orange-500 transition-all ease-in-out delay-150  
+                cursor-pointer border-2 border-orange-500  overflow-hidden ">
+                    <div className="flex">
+                        SERVIÇOS <CaretUp  className="w-6 h-6 ml-3 group-hover:rotate-180 hover:transition-all ease-in-out delay-150" />
+                    </div>
+                    
+                </span> */}
+
+
+                
                 <span className="flex m-2  items-center px-5 rounded-full hover:bg-orange-500 transition-all ease-in-out delay-150  cursor-pointer border-2 border-orange-500">
                     CONTATE-NOS <ChatDots className="w-6 h-6 ml-3" />
                     
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -6,7 +6,7 @@ import { Links } from "./Links";
 export function NavBar() {
     return (
         <div>
-        <nav className="bg-zinc-800 w-screen flex h-20 items-center   text-zinc-100 ">
+        <nav className="bg-zinc-800 w-screen flex h-16 sm:h-20 items-center   text-zinc-100  ">
             {/* <List className="w-10 h-8 cursor-pointer" color="white"  /> */}
                 
             <span className="text-zinc-100 flex items-center font-poppins text-3xl  py-4 px-3 ">TOS</span>
@@ -18,22 +18,55 @@ export function NavBar() {
             <div className="sm:hidden flex items-center">
                 
                 <Popover className="">
-                    <Popover.Panel className="bg-zinc-800 absolute top-24 right-4 p-5 rounded-md">
-                        <span className="flex m-2 items-center  p-3  px-5 rounded-full group hover:bg-orange-500 transition-all ease-in-out delay-150  cursor-pointer border-2 border-orange-500">
+                    <Popover.Panel className="bg-zinc-800 absolute top-24 right-4 p-5 rounded-xl">
+                        <span className="flex m-2 items-center  p-3  px-5 rounded-xl group hover:bg-orange-500 transition-all ease-in-out delay-150  cursor-pointer border-2 border-orange-500">
                         SOBRE-NÓS <Bus className="w-6 h-6 ml-3" />
                         
                         </span>
-                        <span className="flex m-2 items-center p-3 my-4 px-5 rounded-full hover:bg-orange-500 transition-all ease-in-out delay-150  cursor-pointer border-2 border-orange-500">
-                            SERVIÇOS <CaretUp  className="w-6 h-6 ml-3 hover:rotate-180 hover:transition-all ease-in-out delay-150" />
+                        <span className="
+                        flex m-2 
+                        p-3 my-4 px-5
+                         rounded-xl 
+                        cursor-pointer
+                        border-2 
+                        border-orange-500
+                        group
+                        overflow-hidden
+                        flex-col
+                        max-h-12
+                        transition-all ease-in-out delay-450
+                        
+                        hover:max-h-max
+                        ">
+                            <div className="flex">
+                            SERVIÇOS 
+                            <CaretUp  className="w-6 h-6 group-hover:rotate-180 hover:transition-all ease-in-out delay-150 ml-8" />
+                            </div>
+
+                            <div className="flex flex-col pt-5  ">
+                            
+                            <span className="hover:text-zinc-400 cursor-pointer py-2 transition-all">
+                                Lorem Ipsum
+                            </span>
+                            
+                            <span className="hover:text-zinc-400 cursor-pointer py-2 transition-all">
+                                Lorem Ipsum
+                            </span>
+                            
+                            <span className="hover:text-zinc-400 cursor-pointer py-2 transition-all">
+                                Lorem Ipsum
+                            </span>
+                            
+                            </div>
                             
                         </span>
-                        <span className="flex m-2  items-center p-3  px-5 rounded-full hover:bg-orange-500 transition-all ease-in-out delay-150  cursor-pointer border-2 border-orange-500">
+                        <span className="flex m-2  items-center p-3  px-5 rounded-xl hover:bg-orange-500 transition-all ease-in-out delay-150  cursor-pointer border-2 border-orange-500">
                             CONTATE-NOS <ChatDots className="w-6 h-6 ml-3" />
                             
                         </span>
                     </Popover.Panel>
 
-                    <Popover.Button className="absolute top-5  right-2">
+                    <Popover.Button className="absolute top-4  right-2">
                         <List className= "w-10 h-10"/>
                     </Popover.Button>
                 </Popover>
@@ -44,8 +77,8 @@ export function NavBar() {
         </nav>
 
 
-            <span className="">
-
+            <span className="text-zinc-100 font-roboto text-xl m-10">
+                What is Lorem Ipsum?
             </span>
 
         </div>
